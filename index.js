6981ec44c0648f28fe6315b70d3d08bf6b8da631
Patricia Silvestre Alvarenga <patriciasilvestrea@gmail.com>
2021-12-09 10:33:33 -0300

    altera logica

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
